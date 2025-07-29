d4a4b6552e16fe77ac704ff4117981c496bd6faa
Stanislav Suschevici <68298104+KoatKoetl@users.noreply.github.com>
2025-07-29 20:15:13 +0300

    Feature/registration page (#3)
    * (Feature) - styled registration form
    (Feature) - add translation to fields/errors/labels of form
    (Feature) - use zod and react hook forms to validate the form
    
    * (Feature) - add supabase registration connection
    
    * (Feature) - add header register Link
    (Feature) - add theme provide for future changes
    
    * (Feature) - add locally the Supabase Edge function that register users

--- Finance_tracker/src/components/Footer.tsx
+++ Finance_tracker/src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="bg-black">
+    <footer className="bg-black min-h-56px">
       <div className="wrapper flex items-center justify-center p-4 text-white max-w-[1440px] mx-auto">
         <p>© 2025 Finance Tracker. All rights reserved.</p>
       </div>
--- Finance_tracker/src/components/Header.tsx
+++ Finance_tracker/src/components/Header.tsx
@@ -1,13 +1,37 @@
 import LanguageSelector from "./LanguageSelector";
+import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import { Button } from "./ui/button";
+import { UserPlus } from "lucide-react";
 
 const Header = () => {
+  const { t } = useTranslation();
+
   return (
-    <header className="sticky top-0 z-1 border-b-1 border-gray-100 shadow-md">
-      <div className="wrapper relative z-1 flex items-center justify-between p-4 max-w-[1440px] mx-auto">
+    <header className="sticky top-0 z-1 border-b-1 border-gray-100 shadow-md min-h-[70px]">
+      <div className="wrapper relative z-1 flex items-center justify-between px-4 py-1 max-w-[1440px] mx-auto">
         <div className="icon flex items-baseline">
-          <img src="/logo.svg" alt="" className="w-20" />
+          <Link to={"/"}>
+            <img src="/logo.svg" alt="" className="w-20" />
+          </Link>
         </div>
-        <LanguageSelector />
+        <div className="flex gap-2">
+          <div>
+            <Link
+              to={"/register"}
+              className="text-gray-600 hover:text-gray-900"
+            >
+              <Button
+                variant="link"
+                className="text-black cursor-pointer hover:underline"
+              >
+                <UserPlus />
+                {t("registration")}
+              </Button>
+            </Link>
+          </div>
+          <LanguageSelector />
+        </div>
       </div>
     </header>
   );
