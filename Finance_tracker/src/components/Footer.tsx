a81822c1d9f7af4af28865be112648bdf32a22af
Stanislav Suschevici <68298104+KoatKoetl@users.noreply.github.com>
2025-07-29 16:45:13 +0300

    (Feature) - add registration page (#1)

--- Finance_tracker/src/components/Footer.tsx
+++ Finance_tracker/src/components/Footer.tsx
@@ -1,7 +1,9 @@
 const Footer = () => {
   return (
-    <footer>
-      <p>© 2025 Finance Tracker. All rights reserved.</p>
+    <footer className="bg-black">
+      <div className="wrapper flex items-center justify-center p-4 text-white max-w-[1440px] mx-auto">
+        <p>© 2025 Finance Tracker. All rights reserved.</p>
+      </div>
     </footer>
   );
 };
--- Finance_tracker/src/routes/routes.tsx
+++ Finance_tracker/src/routes/routes.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
+import Registration from "../pages/Registration";
 
 const RoutesComponent = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/register" element={<Registration />} />
     </Routes>
   );
 };
