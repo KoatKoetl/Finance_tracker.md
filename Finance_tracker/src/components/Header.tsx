81492c7aa699fd136e59e559ce6a55ea213c2171
KoatKoetl <stas20027@outlook.com>
2025-07-31 19:27:14 +0300

    (FIX) - use zustand to store auth session and use across the app

--- Finance_tracker/src/components/Header.tsx
+++ Finance_tracker/src/components/Header.tsx
@@ -3,9 +3,11 @@ import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import { Button } from "./ui/button";
 import { UserPlus } from "lucide-react";
+import { useAuthStore } from "../stores/AuthStore";
 
 const Header = () => {
   const { t } = useTranslation();
+  const { isAuthenticated } = useAuthStore();
 
   return (
     <header className="sticky top-0 z-1 border-b-1 border-gray-100 shadow-md min-h-[70px]">
@@ -16,20 +18,23 @@ const Header = () => {
           </Link>
         </div>
         <div className="flex gap-2">
-          <div>
-            <Link
-              to={"/register"}
-              className="text-gray-600 hover:text-gray-900"
-            >
-              <Button
-                variant="link"
-                className="text-black cursor-pointer hover:underline"
+          {!isAuthenticated && (
+            <div>
+              <Link
+                to={"/register"}
+                className="text-gray-600 hover:text-gray-900"
               >
-                <UserPlus />
-                {t("registration")}
-              </Button>
-            </Link>
-          </div>
+                <Button
+                  id="register-page-button"
+                  variant="link"
+                  className="text-black cursor-pointer hover:underline"
+                >
+                  <UserPlus />
+                  {t("registration")}
+                </Button>
+              </Link>
+            </div>
+          )}
           <LanguageSelector />
         </div>
       </div>
